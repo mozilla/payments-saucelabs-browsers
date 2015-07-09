660f67f1b4f4b061f275eebf2fc6b7bad879bcd0
Stuart Colville <scolville@mozilla.com>
2015-07-09 17:43:37 +0100

    Add initial version

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  sl_chrome: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    version: '43',
+  },
+  sl_chrome_older: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    version: '42',
+  },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: '39',
+  },
+  sl_firefox_older: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    version: '38',
+  },
+  sl_ie: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    version: '10',
+  },
+  sl_ie_older: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    version: '9',
+  },
+  sl_safari: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    version: '8',
+  },
+  sl_safari_older: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    version: '7',
+  },
+};
